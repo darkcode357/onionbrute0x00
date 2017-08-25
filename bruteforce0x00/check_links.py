83878cbb4a2190e11d176007e6ec5371e19a3fa3
darkcode357 <darkcode357@gmail.com>
2017-08-25 19:19:42 +0200

    Update check_links.py

--- bruteforce0x00/check_links.py
+++ bruteforce0x00/check_links.py
@@ -7,14 +7,27 @@ from os.path import basename
 from bs4 import BeautifulSoup
 from PIL import Image
 from PIL.ExifTags import TAGS
+import socks
+import socket 
+
+
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS4, "127.0.0.1", 9050, True)
+socket.socket = socks.socksocket
 
 
 def findImages(url):
     print ('[+]=> procurando imagens na url => ' + url)
     urlContent = urllib2.urlopen(url).read()
+    try:
+        urlContent = urllib2.urlopen(url).read()
+    except urllib2.HTTPError, e:
+        print e.code
+    except urllib2.URLError, e:
+        print e.args
     soup = BeautifulSoup(urlContent, 'html.parser')
     imgTags = soup.findAll('img')
     return imgTags
+    print("saida")
 
 
 def downloadImage(imgTag):
@@ -49,20 +62,16 @@ def testForExif(imgFileName):
 
 
 def main():
-    parser = optparse.OptionParser('usage backinfogps -u target url')
-    parser.add_option('-u', dest='url', type='string',
-      help='specify url address')
-
-    (options, args) = parser.parse_args()
-    url = options.url
-    if url == None:
-        print (parser.usage)
-        exit(0)
-    else:
-        imgTags = findImages(url)
-        for imgTag in imgTags:
-            imgFileName = downloadImage(imgTag)
-            testForExif(imgFileName)
+    dsa = open("link1.txt", "r+")
+    for url in dsa: 
+    	if url == None:
+        	print ("saida")
+        	exit(0)
+    	else:
+        	imgTags = findImages(url)
+        	for imgTag in imgTags:
+            		imgFileName = downloadImage(imgTag)
+            		testForExif(imgFileName)
 
 
 if __name__ == '__main__':
