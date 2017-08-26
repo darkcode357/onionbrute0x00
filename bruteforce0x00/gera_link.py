af4f99fd04f3542bb894b70e6ee486e4429eca92
darkcode357 <darkcode357@gmail.com>
2017-08-26 04:08:05 +0200

    Update gera_link.py

--- bruteforce0x00/gera_link.py
+++ bruteforce0x00/gera_link.py
@@ -3,16 +3,31 @@ import sys
 from os import system
 
 def gera_senha(tamanho):
-        catacteres = '0123456789abcdefghijlmnopqrstuwvxzABCDEFGHIJLMNOPKQRSTUWVXZ'
+        catacteres = '0123456789abcdefghijlmnopqrstuwvxyz'
         senha = ''
         for char in xrange(tamanho):
                 senha += choice(catacteres)
 	return (senha)
 
+def gera_senha2(tamanho2):
+	caracteres = "ABCDEFGHIJLMNOPQRSTUWVXYZ"
+	senha = ''
+	for char in xrange(tamanho2):
+		senha += choice(caracteres)
+	return (senha) 
+
 while True:
 	a = "http://www."+gera_senha(16)+".onion"+"\n"
+        print(a)
         arquivo = system("touch link1.txt")
-	arquivo = open('link1.txt', 'r+')
+        arquivo = open('link1.txt', 'r+')
+        print(str(arquivo.readlines())) #conseguiler
+        arquivo.write(a)
+############################################################
+	b = "http://www."+gera_senha2(16)+".onion"+"\n"
+	print(b)
+	arquivo = system("touch link2.txt")
+	arquivo = open('link2.txt', 'r+')
 	print(str(arquivo.readlines())) #conseguiler
-	arquivo.write(a)
-arquivo.close()
+	arquivo.write(b)
+#arquivo.close()
